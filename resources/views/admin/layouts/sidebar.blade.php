558b63a27fcbffacc26f0b29e6f14219c106d378
didepanlayar <albinmaulana@yahoo.com>
2025-06-24 14:53:39 +0700

    Feature: Global Layout for Admin with Stisla Admin Template

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,43 @@
+<!-- Navbar Start -->
+@include('admin.layouts.navbar')
+<!-- Navbar End -->
+
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="#">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="#">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="menu-header">Dashboard</li>
+            <li class="active">
+                <a href="#" class="nav-link"><i class="fas fa-fire"></i><span>Dashboard</span></a>
+            </li>
+            <li class="menu-header">Starter</li>
+            <li><a class="nav-link" href="#"><i class="far fa-square"></i> <span>Blank Page</span></a></li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="far fa-file-alt"></i> <span>Forms</span></a>
+                <ul class="dropdown-menu">
+                    <li><a class="nav-link" href="#">Advanced Form</a></li>
+                    <li><a class="nav-link" href="#">Editor</a></li>
+                    <li><a class="nav-link" href="#">Validation</a></li>
+                </ul>
+            </li>
+            <li class="dropdown">
+                <a href="#" class="nav-link has-dropdown"><i class="fas fa-map-marker-alt"></i> <span>Google Maps</span></a>
+                <ul class="dropdown-menu">
+                    <li><a href="#">Advanced Route</a></li>
+                    <li><a href="#">Draggable Marker</a></li>
+                    <li><a href="#">Geocoding</a></li>
+                    <li><a href="#">Geolocation</a></li>
+                    <li><a href="#">Marker</a></li>
+                    <li><a href="#">Multiple Marker</a></li>
+                    <li><a href="#">Route</a></li>
+                    <li><a href="#">Simple</a></li>
+                </ul>
+            </li>
+        </ul>
+    </aside>
+</div>
