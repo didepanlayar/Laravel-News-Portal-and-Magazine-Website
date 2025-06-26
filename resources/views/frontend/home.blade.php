8759bbde1690709aa404a1119453b6f756c4e9f0
didepanlayar <albinmaulana@yahoo.com>
2025-06-26 23:08:32 +0700

    Feature: Setup Global Layout for Frontend

---
+++ resources/views/frontend/home.blade.php
@@ -0,0 +1,29 @@
+@extends('frontend.layouts.app')
+
+@section('title') Home @endsection
+
+@section('content')
+    <!-- Tranding news  carousel-->
+    @include('frontend.components.home.tranding-news')
+    <!-- End Tranding news carousel -->
+
+    <!-- Hero news -->
+    @include('frontend.components.home.hero')
+    <!-- End Hero news -->
+
+    <div class="large_add_banner">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="large_add_banner_img">
+                        <img src="{{ asset('frontend/assets/images/ads.png') }}" alt="adds" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Popular news category -->
+    @include('frontend.components.home.main-news')
+    <!-- End Popular news category -->
+@endsection
