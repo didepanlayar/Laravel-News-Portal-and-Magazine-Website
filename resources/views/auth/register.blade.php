d0c8dc9799b795b4915dd5aaab62864a913480a9
didepanlayar <albinmaulana@yahoo.com>
2025-07-29 13:38:04 +0700

    Feature: Frontend - Register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,52 +1,69 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('register') }}">
-        @csrf
+@extends('frontend.layouts.app')
 
-        <!-- Name -->
-        <div>
-            <x-input-label for="name" :value="__('Name')" />
-            <x-text-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')" required autofocus autocomplete="name" />
-            <x-input-error :messages="$errors->get('name')" class="mt-2" />
+@section('title', 'Register')
+
+@section('content')
+    <!-- register -->
+    <section class="wrap__section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Form Register -->
+                    <div class="card mx-auto" style="max-width: 520px">
+                        <article class="card-body">
+                            <header class="mb-4">
+                                <h4 class="card-title">{{ __('frontend.Register') }}</h4>
+                            </header>
+                            <form action="{{ route('register') }}" method="POST">
+                                @csrf
+                                <div class="form-group">
+                                    <label>{{ __('frontend.Name') }}</label>
+                                    <input type="text" class="form-control" name="name" value="{{ old('name') }}" placeholder="{{ __('frontend.Name') }}" required />
+                                    @error('name')
+                                        <div class="invalid-feedback" style="display: block">{{ $message }}</div>
+                                    @enderror
+                                </div>
+
+                                <div class="form-group">
+                                    <label>{{ __('frontend.Email') }}</label>
+                                    <input type="email" class="form-control" name="email" value="{{ old('email') }}" placeholder="{{ __('frontend.Email') }}" required />
+                                    @error('email')
+                                        <div class="invalid-feedback" style="display: block">{{ $message }}</div>
+                                    @enderror
+                                    <small class="form-text text-muted">{{ __('frontend.We\'ll never share your email with anyone else') }}.</small>
+                                </div>
+
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label>{{ __('frontend.Password') }}</label>
+                                        <input class="form-control" type="password" name="password" required />
+                                    </div>
+
+                                    <div class="form-group col-md-6">
+                                        <label>{{ __('frontend.Repeat Password') }}</label>
+                                        <input class="form-control" type="password" name="password_confirmation" required />
+                                    </div>
+
+                                    <div class="form-group col-md-12">
+                                        @error('password')
+                                            <div class="invalid-feedback" style="display: block">{{ $message }}</div>
+                                        @enderror
+    
+                                        @error('password_confirmation')
+                                            <div class="invalid-feedback" style="display: block">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">{{ __('frontend.Register') }}</button>
+                                </div>
+                            </form>
+                        </article>
+                    </div>
+                    <!-- End Form Register -->
+                </div>
+            </div>
         </div>
-
-        <!-- Email Address -->
-        <div class="mt-4">
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
-
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                            type="password"
-                            name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('login') }}">
-                {{ __('Already registered?') }}
-            </a>
-
-            <x-primary-button class="ms-4">
-                {{ __('Register') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+    </section>
+    <!-- end register -->
+@endsection
