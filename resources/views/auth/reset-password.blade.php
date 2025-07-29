bd9c114839dc33ca7f59bd902d413caddf17133d
didepanlayar <albinmaulana@yahoo.com>
2025-07-29 19:41:44 +0700

    Feature: Frontend - Reset Password

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -1,39 +1,63 @@
-<x-guest-layout>
-    <form method="POST" action="{{ route('password.store') }}">
-        @csrf
+@extends('frontend.layouts.app')
 
-        <!-- Password Reset Token -->
-        <input type="hidden" name="token" value="{{ $request->route('token') }}">
+@section('title', 'Register')
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email', $request->email)" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+@section('content')
+    <!-- Reset -->
+    <section class="wrap__section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <!-- Form Reset -->
+                    <div class="card mx-auto" style="max-width: 520px">
+                        <article class="card-body">
+                            <header class="mb-4">
+                                <h4 class="card-title">{{ __('frontend.Reset Password') }}</h4>
+                            </header>
+                            <form action="{{ route('password.store') }}" method="POST">
+                                @csrf
+                                <input type="hidden" name="token" value="{{ $request->route('token') }}">
+
+                                <div class="form-group">
+                                    <label>{{ __('frontend.Email') }}</label>
+                                    <input type="email" class="form-control" name="email" value="{{ old('email', $request->email) }}" placeholder="{{ __('frontend.Email') }}" required />
+                                    @error('email')
+                                        <div class="invalid-feedback" style="display: block">{{ $message }}</div>
+                                    @enderror
+                                </div>
+
+                                <div class="form-row">
+                                    <div class="form-group col-md-6">
+                                        <label>{{ __('frontend.Password') }}</label>
+                                        <input class="form-control" type="password" name="password" required />
+                                    </div>
+
+                                    <div class="form-group col-md-6">
+                                        <label>{{ __('frontend.Repeat Password') }}</label>
+                                        <input class="form-control" type="password" name="password_confirmation" required />
+                                    </div>
+
+                                    <div class="form-group col-md-12">
+                                        @error('password')
+                                            <div class="invalid-feedback" style="display: block">{{ $message }}</div>
+                                        @enderror
+    
+                                        @error('password_confirmation')
+                                            <div class="invalid-feedback" style="display: block">{{ $message }}</div>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="form-group">
+                                    <button type="submit" class="btn btn-primary btn-block">{{ __('frontend.Reset Password') }}</button>
+                                </div>
+                            </form>
+                        </article>
+                    </div>
+                    <!-- End Form Reset -->
+                </div>
+            </div>
         </div>
+    </section>
+    <!-- end Reset -->
+@endsection
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
-            <x-text-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="new-password" />
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Confirm Password -->
-        <div class="mt-4">
-            <x-input-label for="password_confirmation" :value="__('Confirm Password')" />
-
-            <x-text-input id="password_confirmation" class="block mt-1 w-full"
-                                type="password"
-                                name="password_confirmation" required autocomplete="new-password" />
-
-            <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Reset Password') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
